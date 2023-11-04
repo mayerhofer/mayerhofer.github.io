c71092ccdd8dcb04a8a9da35b944fb0c8d3c8443
Ricardo Mayerhofer <ricardo.mayerhofer@edreamsodigeo.com>
2023-11-04 01:03:09 +0100

    Back button epic fixes

--- src/js/busCompts/backButton.js
+++ src/js/busCompts/backButton.js
@@ -8,7 +8,7 @@ export default class BackButton extends RComponent {
     {
       id: this.id,
       disabled: '',
-      class name: 'financeSave',
+      className: 'financeSave',
       img: "iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAABNUlEQVR4nO2WvUoDQRSFv5ewUBOxtIiVKfUBBA0+R0zis9gpBgtLe3/eJCv2inZu7ExQFm4giDN75u5GUuTAKXaZ4eP+zJ2BlZZQW0AfeAQy4NOc2b8e0KwTuAlcAhPgu8RT4BbYrgo9AcYC8LdzoOOFnlkEqdD56AeeSKcVoPNwOfKGM72xtG8o4OsaoTNfKUemrHvfgD1gPwE8sUwG1RegLVvbToz6NAZ+EKEt+04B38XAz4FN78CurdkBXh11zmLgPLCpqKmqUO3zRYMPPOAnMdUvjlSPlrK5egs8Tt0YuCkOkHakln/5q2yAFBo66lfmC8SLP9TdHn8A64g6FF8cyrV4rEJnGtTwEChmv0sdZ9qL9B5RUWvAuXWmEuVNSk0VNexqu7cpNDaPbDh0lSOzEv+tH+YHlQGe3ZAGAAAAAElFTkSuQmCC"
     });
   }
--- src/js/main.js
+++ src/js/main.js
@@ -175,12 +175,12 @@ class GenericTable extends RComponent {
   }
 
   render() {
-    if (this.showEdit) {
+    if (this.state.showEdit) {
       const handleInsert = this.handleInsertOne.bind(this);
       const handleUpdate = this.handleUpdateOne.bind(this);
       const content = this.fill('scrollDiv', {
         id: this.id + 'content', 
-        content: this.buildRComponent({id: this.props.idEditCompoment, handleInsert, handleUpdtate, handleClose: this.handleClose.bind(this), data, element: this.state.editing}, this.props.buildEditComponent)
+        content: this.buildRComponent({id: this.props.idEditComponent, handleInsert, handleUpdate, handleClose: this.handleClose.bind(this), data: this.state.data, element: this.state.editing}, this.props.buildEditComponent)
       });
 
       return this.fill('div', {id: this.id, className: 'table__wrapper', content: content });
@@ -558,7 +558,7 @@ window.app.loadLiability = function() {
       entity: 'liability',
       handleSelect,
       idEditComponent: 'liabilityForm',
-      buildEditComponent: p => new Liability form(p),
+      buildEditComponent: p => new LiabilityForm(p),
       formatter: {
         ...commonFormatters,
         provider: (row) => {
